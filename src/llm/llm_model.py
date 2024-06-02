f66701714b797c15ccd18f2810b59004cf3e716e
samuel <samueloliveira32df@gmail.com>
2024-06-02 17:20:00 -0300

    llm-marketplace (docs): update marketplace jouney docs

--- src/llm/llm_model.py
+++ src/llm/llm_model.py
@@ -27,6 +27,25 @@ logging.basicConfig(
 
 
 class MarketplaceJourney:
+    """
+    Manages the lifecycle of a marketplace interaction using a language model.
+
+    This class orchestrates the interaction flow, handling user queries about products,
+    managing state transitions based on those queries, and logging the conversation history.
+
+    Attributes:
+        llm (ChatOpenAI): The language model used for generating text-based responses.
+        session_id (str): A unique identifier for the session.
+        history (ChatMessageHistory): Records the history of messages in the session.
+        document_manager (DocumentManager): Manages the retrieval and formatting of product details.
+        chatbot (MarketplaceChatbot): The chatbot handling the conversation logic.
+        state_agent (MarketplaceAgent): Manages state transitions within the conversation.
+        main_prompt_template (PromptTemplate): The template for generating conversation prompts.
+        main_chain (LLMChain): The chain that processes the main interaction logic.
+        chain_with_history (RunnableWithMessageHistory): A runnable chain that
+        incorporates message history.
+    """
+
     def __init__(self, retriever: Chroma, llm_type="gpt-3.5-turbo"):
         self.llm = ChatOpenAI(model_name=llm_type, temperature=0)
         self.session_id = str(uuid.uuid4())
@@ -54,7 +73,7 @@ class MarketplaceJourney:
             history_messages_key="chat_history",
         )
 
-    def add_to_history(self, sender, message):
+    def add_to_history(self, sender: str, message: str):
         if sender == "user":
             self.history.add_user_message(message)
         else:
@@ -72,9 +91,9 @@ class MarketplaceJourney:
                 for message in self.history.messages
             ]
         )
-        output_dir = "/07_model_output/"
+        output_dir = "./07_model_output"
         os.makedirs(output_dir, exist_ok=True)
-        filename = f"{output_dir}{self.session_id}.csv"
+        filename = f"{output_dir}/{self.session_id}.csv"
         df.to_csv(filename, index=False, encoding="utf-8")
         logging.info(f"Memory saved to {filename}")
 
@@ -82,14 +101,15 @@ class MarketplaceJourney:
         self.history.clear()
 
     def run_interaction(self, question: str, document: str) -> Optional[str]:
+        """Executes the interaction with the LLM, processing the given question and document details."""
         try:
             response = self.chain_with_history.invoke(
                 {"question": question, "document": document},
                 {"configurable": {"session_id": self.session_id}},
             )
         except AttributeError as e:
-            logging.error(f"Erro ao acessar .messages: {str(e)}")
-            response = None
+            logging.error(f"Error during LLM interaction: {str(e)}")
+            response = "No response available."
             raise
         return response
 
@@ -97,27 +117,20 @@ class MarketplaceJourney:
         self.save_history_to_file()
         self.clear_history()
 
-    def update_prompt(self, next_prompt):
+    def update_prompt(self, next_prompt: PromptTemplate):
         self.main_prompt_template = next_prompt
         self.main_chain.prompt = self.main_prompt_template
 
     def get_answer(self, question: str) -> Tuple[str, Optional[str]]:
         """
-        Get an answer from the LLM based on the stage of interaction.
+        Retrieves an answer from the LLM based on the stage of interaction.
 
-        This function retrieves relevant documents and formats them, then uses
-        different chains to get an answer based on the stage (intro, main, end).
+        Parameters:
+            question (str): The question asked by the user.
 
-        Parameters
-        ----------
-        question : str
-            The question asked by the user.
-
-        Returns
-        -------
-        Tuple[str, Optional[str]]
-            A tuple containing the response message and the formatted documents
-            (if applicable).
+        Returns:
+            Tuple[str, Optional[str]]: A tuple containing the response
+            message and the formatted documents.
         """
         self.add_to_history("user", question)
         formatted_docs = self.document_manager.get_product_details(question)
